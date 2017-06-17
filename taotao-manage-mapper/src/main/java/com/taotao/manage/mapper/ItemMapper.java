cbdbf7128753fbad81fc1bb6d262868073705e6e
liangdong <youbestld@qq.com>
2017-06-17 17:47:44 +0800

    manage

--- taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/ItemMapper.java
+++ taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/ItemMapper.java
@@ -2,7 +2,9 @@ package com.taotao.manage.mapper;
 
 import com.github.abel533.mapper.Mapper;
 import com.taotao.manage.pojo.Item;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemMapper extends Mapper<Item>{
 
 }
--- taotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
+++ taotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
@@ -1,8 +1,12 @@
 package com.taotao.manage.service;
 
+import java.io.IOException;
 import java.util.List;
 
+import com.taotao.common.httpclient.HttpResult;
+import com.taotao.common.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.github.abel533.entity.Example;
@@ -16,49 +20,55 @@ import com.taotao.manage.pojo.ItemParamItem;
 
 @Service
 public class ItemService extends BaseService<Item> {
-    
+
     @Autowired
     private ItemDescService itemDescService;
-    
+
     @Autowired
     private ItemMapper itemMapper;
-    
+
+    @Autowired
+    private ApiService apiService;
+
     @Autowired
     private ItemParamItemService itemParamItemService;
-    
+
+    @Value("${TAOTAO_WEB_URL}")
+    private String TAOTAO_WEB_URL;
+
     public boolean saveItem(Item item,String desc,String itemParams){
         item.setStatus(1);
         item.setId(null);//出于安全考虑，防止有人恶意出入id参数，将id赋值为Null
         Integer count1 = super.save(item);
-        
+
         ItemDesc itemDesc = new ItemDesc();
         itemDesc.setItemId(item.getId());
         itemDesc.setItemDesc(desc);
         Integer count2 = itemDescService.save(itemDesc);
-        
+
         //保存规格参数数据
         ItemParamItem itemParamItem = new ItemParamItem();
         itemParamItem.setItemId(item.getId());
         itemParamItem.setParamData(itemParams);
         Integer count3 = this.itemParamItemService.save(itemParamItem);
-        
+
         return count1==1 && count2==1 && count3==1;
-        
+
     }
-    
+
     public EasyUIResult queryItemList(Integer page,Integer rows){
-        
+
         //设置分页数据
         PageHelper.startPage(page, rows);
-        
+
         Example example = new Example(Item.class);
         example.setOrderByClause("created DESC");
         List<Item> items = this.itemMapper.selectByExample(example);
         PageInfo<Item> pageInfo = new PageInfo<Item>(items);
-        
+
         return new EasyUIResult(pageInfo.getTotal(),pageInfo.getList());
     }
-    
+
     public boolean updateItem(Item item,String desc,String itemParams){
         item.setStatus(null);
         Integer count1 = super.updateSelective(item);
@@ -67,6 +77,13 @@ public class ItemService extends BaseService<Item> {
         itemDesc.setItemId(item.getId());
         Integer count2 = this.itemDescService.updateSelective(itemDesc);
         Integer count3 = this.itemParamItemService.updateItemParamItem(item.getId(),itemParams);
+        try {
+            //通知其他系统改商品已经更新
+            String url = TAOTAO_WEB_URL+"/item/cache"+item.getId()+".html";
+            this.apiService.doPost(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return count1.intValue()>0 && count2.intValue()>0 && count3.intValue()>0;
     }
 }
